ffa2ab0beb3701f3db7080cdbffe48dcad6e77a3
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-26 10:14:21 +0700

    menambahkan fungsi untuk input panjang dan lebar.

--- FungsiSwitch/FungsiSwitch.cpp
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -15,5 +15,9 @@ int fungsiLuas(int p, int l)
 
 int main()
 {
-    
+    int panjang, lebar;
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+    cout << "Masukkan lebar: ";
+    cin >> lebar;
 }
